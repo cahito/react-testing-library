6dce96cfdfa5fcf14e6bbc5f5aeaa4ddce6006c0
cahito <cahito@gmail.com>
2022-03-23 19:15:33 -0300

    Quarto requisito cumprido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import NotFound from '../components/NotFound';
+import claudio from '../services/claudio';
+
+const SRC_URL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+describe('Teste do componente "NotFound.js"', () => {
+  it('Testa se a página contém um heading h2 com o texto'
+  + ' "Page requested not found 😭"', () => {
+    claudio(<NotFound />);
+    const notFoundTitle = screen.getByRole('heading', { name: /page requested not/i });
+
+    expect(notFoundTitle).toBeDefined();
+  });
+
+  it('Testa se página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    claudio(<NotFound />);
+    const gifNotFound = screen.getByRole('img', { name: /pikachu crying/i });
+
+    expect(gifNotFound).toHaveAttribute('src', SRC_URL);
+  });
+});
