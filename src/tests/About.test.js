4742766ba5993704d1857e080bd0c1d4fb034919
cahito <cahito@gmail.com>
2022-03-23 16:58:17 -0300

    Segundo requisito cumprido

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { About } from '../components';
 import claudio from '../services/claudio';
 
+const TWO = 2;
+const SRC_URL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+
 describe('Teste do componente "About.js"', () => {
   it('Testa se a página contém as informações sobre a Pokédex', () => {
     claudio(<About />);
-    const textOneAbout = screen.getByText(`/application encyclopedia/i`);
-    const textTwoAbout = screen.getByText(`/filter type details/i`);
+    const textOneAbout = screen.getByText(/application simulates a pokédex/i);
+    const textTwoAbout = screen.getByText(/see more details for each/i);
 
     expect(textOneAbout).toBeInTheDocument();
     expect(textTwoAbout).toBeInTheDocument();
@@ -20,11 +23,19 @@ describe('Teste do componente "About.js"', () => {
     expect(aboutTitle).toBeDefined();
   });
 
-  /* it('Testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+  it('Testa se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+    claudio(<About />);
+    const paragraphs = screen.getAllByText(/Pokémons/);
 
+    expect(paragraphs.length).toBe(TWO);
   });
 
   it('Testa se a página contém uma determinada imagem de uma Pokédex', () => {
+    claudio(<About />);
+    const image = screen.getByAltText(/Pokédex/);
 
-  }); */
+    // .toHaveAttribute veio da sugestão do VSCode e do gitHub do RTL em
+    // https://github.com/testing-library/react-testing-library/issues/43
+    expect(image).toHaveAttribute('src', SRC_URL);
+  });
 });
